d96441727512182ee7d52775f320e6328432ea54
ynhof6 <59070043@it.kmitl.ac.th>
2019-11-16 00:50:22 +0700

    Backend selection page complete
    hard code(will fix later)

---
+++ Backend/bank.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title></title>
+</head>
+<body>
+	<?php 
+		echo "string";
+    	$ticket = $_POST['type'];
+    	echo $ticket;
+    ?>
+</body>
+</html>
